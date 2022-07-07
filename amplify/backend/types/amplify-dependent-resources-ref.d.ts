c31b7ccf2b04f84553fdf4ba445567f4623ef2fe
primofin <vynguyen9699@gmail.com>
2022-07-07 22:57:18 +0300

    configure aws amplify auth and add authentication to the React App

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplify186a990e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
