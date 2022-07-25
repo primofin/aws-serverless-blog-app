254fd63a6727e8fcb4266ba54e0b807e5b34cb63
primofin <vynguyen9699@gmail.com>
2022-07-25 20:22:00 +0300

    initiate redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import './stylesheets/main.scss';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './redux/store';
+
+import './stylesheets/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
