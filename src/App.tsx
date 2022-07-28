5b81666b2af8a51d01f27658d4e3f9e8c959deec
primofin <vynguyen9699@gmail.com>
2022-07-28 23:52:49 +0300

    remove testing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { API, graphqlOperation } from 'aws-amplify';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { createUser } from './graphql/mutations';
+import { useDispatch } from 'react-redux';
 import { getCurrentUser } from './redux/slices/authSlice';
-import { AppDispatch, RootState } from './redux/store';
+import { AppDispatch } from './redux/store';
 import AppRouter from './Routes';
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
-  const { user } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     dispatch(getCurrentUser());
-    if (user) {
-      console.log('a');
-      API.graphql(graphqlOperation(createUser, { input: user }));
-    }
   }, []);
 
   return (
